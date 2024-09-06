fffb09baf672c4b0517d21a0ca530242f5f25fbb
BeomjunCho <chobj555@gmail.com>
2024-09-06 12:11:02 -0700

    Class 1 visual studio code
    Basic project with basic lines

---
+++ GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_Cho_Beomjun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Jun Cho ENTA 1133");
+        }
+    }
+}

bd3ac04a3d60ae95115d9aaef487bcceaafee02b
BeomjunCho <chobj555@gmail.com>
2024-09-06 15:06:52 -0700

    Class 1 assignment revision

--- GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Program.cs
+++ GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Program.cs
@@ -5,9 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! My name is Jun Cho, today is Sep 6th 2024 enta 1133");
-            Console.WriteLine("Decimal number's values are stored and processed by a computer in  a decimal representation which is a base 10 representation: 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
-            Console.WriteLine("Hexadecimal number's values are stored and processed by a computer in hexadecimal representation which is a base 16 representation: 0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
-            Console.WriteLine("Binary number's values are stored and processed by a computer in Binary representation which is a base 2 representation : 0000, 0001, 0010, 0100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("Decimal number's values are stored and processed by a computer in  a decimal representation which is a base 10 representation.");
+            Console.WriteLine("0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("Hexadecimal number's values are stored and processed by a computer in hexadecimal representation which is a base 16 representation.");
+            Console.WriteLine("0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
+            Console.WriteLine("Binary number's values are stored and processed by a computer in Binary representation which is a base 2 representation.");
+            Console.WriteLine("00000000, 00000001, 00000010, 00000100, 00001000,r 0010000, 00100000, 01000000, 01100100, 11111111");
             Console.WriteLine("Good bye!");
         }
     }

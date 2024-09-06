216c37a747c06c964d328ae6f6a81926b426bff3
BeomjunCho <chobj555@gmail.com>
2024-09-06 14:25:14 -0700

    Class 1 assignment
    Posted decimal, hex, binary numbers

--- GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Program.cs
+++ GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Jun Cho ENTA 1133");
+            Console.WriteLine("Hello! My name is Jun Cho, today is Sep 6th 2024 enta 1133");
+            Console.WriteLine("Decimal number: 0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("Hexadecimal number: 0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
+            Console.WriteLine("Binary number: 0000, 0001, 0010, 0100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("Good bye!");
         }
     }
 }

5ffbcf577c12361af26645eb379869bbc98bcc69
BeomjunCho <chobj555@gmail.com>
2024-09-19 18:36:57 -0700

    Class 2 Assignment
    Main programming

---
+++ GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Scripts/DieRoller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_Lab1_Cho_Beomjun.Scripts
+{
+    public class DieRoller
+    {
+        public int numberOfSides = 6;
+
+        public void Description()
+        {
+            Console.WriteLine("Welcome! Jun 240919");
+            Console.WriteLine("This is a game about rolling dice with " + numberOfSides + " sides");
+            Console.WriteLine("Min Roll:");
+            Console.WriteLine(1);
+            Console.WriteLine("Max Roll:");
+            Console.WriteLine(numberOfSides);
+        }
+
+        public int RollDice()
+        {
+            Random rnd = new Random();
+            int RanNum = rnd.Next(1, numberOfSides + 1);
+            return RanNum;
+        }
+
+        public int TotalScores()
+        {
+            int Total = 0;
+            int Num = 1;
+            for (int i = 0; i < 4; i++)
+            {   
+                Total += RollDice();
+                Console.WriteLine("Dice number " + (Num + i) + ":" + RollDice());
+            }
+            return Total;
+        }
+    }
+}
+
---
+++ GD12_1133_Lab1_Cho_Beomjun/GD12_1133_Lab1_Cho_Beomjun/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using GD12_1133_Lab1_Cho_Beomjun.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_Lab1_Cho_Beomjun.Scripts
+{
+    /*internal class GameController
+    {
+        public static void ProgramStart()
+        {
+
+        }
+    }*/
+
+}
+
+internal class GameController
+{
+    public static void ProgramStart()
+    {
+        DieRoller dieRoller = new DieRoller();
+        dieRoller.Description();
+        int total = dieRoller.TotalScores();
+        Console.WriteLine("Your total score is " + total);
+
+        Console.WriteLine(" -----------------------------------------------------");
+        Console.WriteLine("|Research and Explanation about aristhmeic operators. |");
+        Console.WriteLine(" -----------------------------------------------------");
+        Console.WriteLine("Addition operator + : Adds together two values");
+        Console.WriteLine("Subtraction operator - : Subtracts one value from another");
+        Console.WriteLine("Division operator / : Divides one value by another");
+        Console.WriteLine("Multiplication operator * : Multiplies two values");
+        Console.WriteLine("Increment operator ++ : Increases the value of a variable by 1");
+        Console.WriteLine("Decrement operator -- : Decreases the value of a variable by 1");
+        Console.WriteLine("Remainder operator % : Computes the remainder after dividing its left-hand operand by its right-hand operand.");
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Console.WriteLine("Good bye!");
+    }
+
+}
+
